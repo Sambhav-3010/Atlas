ea8a00c981336b637515b716ac1451bafcb3e2de
= <=>
2025-01-25 09:41:05 +0530

    Help

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./comps/Navbar"; // ✅ Corrected path
+import Home from "./Home";
+import About from "./About";
 import "./App.css";
+import ShapeBlur from './comps/ShapeBlur'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <ShapeBlur className="shape-blur-logo" />
+    </Router>
   );
 }
 
