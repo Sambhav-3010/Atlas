a44a6a5a3d8288bb979ecb64fc330bd57b9270c9
= <=>
2025-01-25 20:45:49 +0530

    Final Tweaks

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className="w-full">
       <div className="text-white w-full mt-10 p-18 rounded-2xl backdrop-blur-md shadow-md font-bold font-sans">
-        <h1 className="text-5xl mb-10">YOU COULD BE A</h1>
+        <h1 className="text-5xl mb-10">YOU COULD BE </h1>
         <FocusCardsDemo />
         <br></br>
         <h2 className="text-4xl font-light">
--- frontend/src/components/ui/FocusCards.jsx
+++ frontend/src/components/ui/FocusCards.jsx
@@ -46,11 +46,11 @@ const FocusCards = ({ cards }) => {
 const FocusCardsDemo = () => {
   const cards = [
     {
-      title: "Mindfulness Retreat in the Woods",
+      title: "A Mindful Monk ",
       src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "A Wellness Escape",
+      title: "A Thrill-seeking Soul",
       src: "https://images.unsplash.com/photo-1600271772470-bd22a42787b3?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
@@ -58,15 +58,15 @@ const FocusCardsDemo = () => {
       src: "https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "A Camper",
+      title: "A Responsible Camper",
       src: "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Nature Lover",
+      title: "A Nature Lover",
       src: "https://images.unsplash.com/photo-1507041957456-9c397ce39c97?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      title: "Solo Traveler",
+      title: "A Solo Adventurer",
       src: "https://assets.aceternity.com/the-first-rule.png",
     },
   ];
